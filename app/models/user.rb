1b0ec08ba530e68f511cca58522745d15c36e931
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2021-09-07 17:13:53 -0300

    feature/create-wallet Add a create wallet
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :wallets, class_name: '::Wallet', inverse_of: :user
+
   validates :name, presence: true
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,5 +1,5 @@
 class Wallet < ApplicationRecord
-  has_one :users, class_name: '::User', inverse_of: :wallet
+  belongs_to :user, class_name: '::User'
 
   validates :name, :description, :status,  presence: true
 end
