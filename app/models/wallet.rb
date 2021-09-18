5074d995d568e0aeda106b7c412bb371cfdaaabb
Tiago <tiagohenriqueodoia@gmail.com>
2021-09-18 17:15:21 -0300

    add-a-coverage-tests Add acoverage test (#22)
    - add a enum to statu and types
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -3,4 +3,7 @@ class Wallet < ApplicationRecord
   has_many :financial_transaction, class_name: '::FinancialTransaction', inverse_of: :wallet
 
   validates :name, :description, :status,  presence: true
+
+
+  enum status: { inactive: 0, active: 1 }
 end
