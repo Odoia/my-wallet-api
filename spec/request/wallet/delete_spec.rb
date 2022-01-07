dcbc28ac6c853993e728833906a77f86d9b3ae62
victor.vieira <victor.vieira@pipefy.com>
2022-01-07 12:08:10 -0300

    Updated delete_spec tests. Added route DELETE wallet/:id. Implemented controller logic for deleting inactive wallets. Also created a delete services

--- spec/request/wallet/delete_spec.rb
+++ spec/request/wallet/delete_spec.rb
@@ -6,24 +6,24 @@ describe '::Api::V1::WalletController', type: :request do
     I18n.default_locale = :en
   end
 
-  let(:body) { JSON.parse response.body }
+  let(:status) { response.status }
 
   context 'When deleting a wallet' do
     context 'When showing a specific wallet' do
       context 'When use a valid id' do
         context 'When use a active wallet' do
-          it 'must return status 200' do
+          it 'must return status 404' do
             wallet = FactoryBot.create(:active_wallet)
             delete "/api/v1/wallet/#{wallet.id}", headers: { 'ACCEPT' => 'application/json' }
-            expect(body['status']).to eq 200
+            expect(status).to eq 404
           end
         end
 
         context 'When use a inactive wallet' do
-          it 'must return status 404' do
+          it 'must return status 200' do
             wallet = FactoryBot.create(:inactive_wallet)
             delete "/api/v1/wallet/#{wallet.id}", headers: { 'ACCEPT' => 'application/json' }
-            expect(body['status']).to eq 404
+            expect(status).to eq 200
           end
         end
       end
@@ -32,7 +32,7 @@ describe '::Api::V1::WalletController', type: :request do
     context 'When use a wallet that doesnt exists' do
         it 'must return status 404' do
             delete '/api/v1/wallet/1', headers: { 'ACCEPT' => 'application/json' }
-            expect(body['status']).to eq 404
+            expect(status).to eq 404
         end
     end
   end
