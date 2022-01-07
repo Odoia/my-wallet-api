81eac7862b12948cf5ae3a2eae6d8ed8743b3cf4
victor.vieira <victor.vieira@pipefy.com>
2022-01-07 13:30:19 -0300

    Formatting delete file

--- app/services/wallet_services/delete.rb
+++ app/services/wallet_services/delete.rb
@@ -1,20 +1,19 @@
 module WalletServices
-    class Delete
-      def initialize(wallet)
-        @wallet         = wallet
-      end
-  
-      def call
-        wallet_delete
-      end
-  
-      private
-  
-      attr_reader :wallet
-  
-      def wallet_delete
-        wallet.delete
-      end
+  class Delete
+    def initialize(wallet)
+      @wallet         = wallet
+    end
+
+    def call
+      wallet_delete
+    end
+
+    private
+
+    attr_reader :wallet
+
+    def wallet_delete
+      wallet.delete
     end
   end
-  
+end
