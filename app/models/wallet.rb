ef3854797e3ca2c4c2fe76ba07d9ecff055e4f16
victor.vieira <victor.vieira@pipefy.com>
2022-01-10 17:27:45 -0300

    Renaming function name

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -6,11 +6,11 @@ class Wallet < ApplicationRecord
 
   enum status: { inactive: 0, active: 1 }
 
-  def self.find_active_wallet_by(id)
+  def self.find_active_wallet_by_id(id)
     find_by(id: id, status: 1, deleted_at: nil)
   end
 
-  def self.find_inactive_wallet_by(id)
+  def self.find_inactive_wallet_by_id(id)
     find_by(id: id, status: 0, deleted_at: nil)
   end
 end
